bbd093f7810bd6473b23e5d36e6ec47be60002f3
Mylene <pont.mylene@gmail.com>
2021-12-08 19:44:27 +0900

    first props and Hello world

--- workshop/src/index.jsx
+++ workshop/src/index.jsx
@@ -1,1 +1,12 @@
 import '../assets/stylesheets/application.scss';
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const Hello = (props) => {
+  const { name, age } = props;
+  const message = `Hello I'm ${name} and I'm ${age}!`;
+  return <h1>{message}</h1>;
+};
+
+const root = document.getElementById('root');
+ReactDOM.render(<Hello name="Jesse" age="32" />, root);
