b71aa3b492547a46429ac9ca774d200b3c5a8aa7
Mylene <pont.mylene@gmail.com>
2021-12-08 19:51:50 +0900

    class component methodd, but functionnal is best

--- workshop/src/index.jsx
+++ workshop/src/index.jsx
@@ -2,11 +2,21 @@ import '../assets/stylesheets/application.scss';
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const Hello = (props) => {
-  const { name, age } = props;
-  const message = `Hello I'm ${name} and I'm ${age}!`;
-  return <h1>{message}</h1>;
-};
+// Functionnal component (JS function to make a component)
+// const Hello = (props) => {
+//   const { name, age } = props;
+//   const message = `Hello I'm ${name} and I'm ${age}!`;
+//   return <h1>{message}</h1>;
+// };
+
+// eslint-disable-next-line react/prefer-stateless-function
+class Hello extends React.Component {
+  render() {
+    const { name, age } = this.props;
+    const message = `Hello I'm ${name} and I'm ${age}!`;
+    return <h1>{message}</h1>;
+  }
+}
 
 const root = document.getElementById('root');
-ReactDOM.render(<Hello name="Jesse" age="32" />, root);
+ReactDOM.render(<Hello name="Jesse" age="21" />, root);
